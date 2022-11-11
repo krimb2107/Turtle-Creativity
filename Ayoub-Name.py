c5f4df876589814996d9ef1dabb50ec7b2da9aec
karima kandil <karima.kandil@um5r.ac.ma>
2022-11-11 18:01:33 +0100

    Dépot 01

---
+++ Ayoub-Name.py
@@ -0,0 +1,65 @@
+import turtle as t
+t.shape("turtle")
+t.bgcolor("black")
+t.fd(-150)
+
+#lettre A
+t.pensize(10)
+t.pencolor('green')
+t.forward(-100)
+t.right(140)
+t.backward(-40)
+t.backward(110)
+t.right(-100)
+t.forward(100)
+t.backward(100)
+
+#Space 1
+t.left(40)
+t.penup()
+t.fd(90)
+t.pendown()
+
+#lettre Y 
+t.right(45)
+t.forward(50)
+t.right(85)
+t.forward(40)
+t.backward(100)
+
+#Space 2
+t.left(60)
+t.penup()
+t.fd(60)
+t.pendown()
+
+#lettre O 
+t.penup()
+t.pendown()
+t.circle(40)
+
+#Space 3
+t.left(90)
+t.penup()
+t.fd(130)
+t.pendown()
+
+#lettre U 
+t.right(110)
+t.forward(40)
+t.circle(35,180)
+t.forward(40)
+t.backward(40)
+
+#Space 4
+t.right(90)
+t.penup()
+t.fd(50)
+t.pendown()
+
+#lettre B
+t.circle(30,180)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.circle(27,180)
